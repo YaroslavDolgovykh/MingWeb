6e269415360253432c290ca3a00d821ea9845f91
yaroslav.dolgovykh@gmail.com <yaroslav.dolgovykh@gmail.com>
2020-05-02 02:41:03 -0700

    init commit

---
+++ MingWeb/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  MingWeb
+//
+//  Created by yar on 28.04.2020.
+//  Copyright © 2020 yar. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.navigationDelegate = self
+        if let url = URL(string: "https://www.mingming.io") {
+            let urlRequest = URLRequest(url: url)
+            webView.load(urlRequest)
+        }
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if navigationAction.navigationType == .linkActivated  {
+            if let url = navigationAction.request.url,
+                let host = url.host, !host.hasPrefix("mingming.io"),
+                UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url)
+                print(url)
+                print("Redirected to browser. No need to open it locally")
+                decisionHandler(.cancel)
+            } else {
+                print("Open it locally")
+                decisionHandler(.allow)
+            }
+        } else {
+            print("not a user click")
+            decisionHandler(.allow)
+        }
+    }
+
+
+}
+
